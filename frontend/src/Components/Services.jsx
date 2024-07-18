eac82a5a4ae6401f5bcdfed75fbed9da0cf1a3a4
Mohd Tanveer Khan <khantanveer6077@gmail.com>
2024-07-19 00:59:33 +0530

    update

--- frontend/src/Components/Services.jsx
+++ frontend/src/Components/Services.jsx
@@ -2,8 +2,19 @@
 
 const Services = () => {
   return (
-    <div>
-      
+    <div  id="Services" className="p-20 flex flex-col items-center justify-center ">
+      <h1 data-aos="fade-right" className="text-[52px] font-semibold leading-normal uppercase text-fuchsia-800 ">Services</h1>
+      <div className="grid lg:grid-cols-5 md:grid-cols-2 grid-cols-1 justify-around gap-20 py-10">
+        <h2 data-aos="fade-up" className="text-[26px] flex items-center justify-center font-senibold  text-fuchsia-800 rounded-3xl h-36 w-44 border-2 border-fuchsia-800 b_glow">HTML</h2>
+        <h2 data-aos="fade-down" className="text-[26px] flex items-center justify-center font-senibold  text-fuchsia-800 rounded-3xl h-36 w-44 border-2 border-fuchsia-800 b_glow">CSS</h2>
+        <h2 data-aos="fade-up" className="text-[26px] flex items-center justify-center font-senibold  text-fuchsia-800 rounded-3xl h-36 w-44 border-2 border-fuchsia-800 b_glow">Bootstrap</h2>
+        <h2 data-aos="fade-down" className="text-[26px] flex items-center justify-center font-senibold  text-fuchsia-800 rounded-3xl h-36 w-44 border-2 border-fuchsia-800 b_glow">Tailwind CSS</h2>
+        <h2 data-aos="fade-up" className="text-[26px] flex items-center justify-center font-senibold  text-fuchsia-800 rounded-3xl h-36 w-44 border-2 border-fuchsia-800 b_glow">Javascript</h2>
+        <h2 data-aos="fade-down" className="text-[26px] flex items-center justify-center font-senibold  text-fuchsia-800 rounded-3xl h-36 w-44 border-2 border-fuchsia-800 b_glow">React</h2>
+        <h2 data-aos="fade-up" className="text-[26px] flex items-center justify-center font-senibold  text-fuchsia-800 rounded-3xl h-36 w-44 border-2 border-fuchsia-800 b_glow">Express</h2>
+        <h2 data-aos="fade-down" className="text-[26px] flex items-center justify-center font-senibold  text-fuchsia-800 rounded-3xl h-36 w-44 border-2 border-fuchsia-800 b_glow">NodeJs</h2>
+        <h2 data-aos="fade-up" className="text-[26px] flex items-center justify-center font-senibold  text-fuchsia-800 rounded-3xl h-36 w-44 border-2 border-fuchsia-800 b_glow">MongoDB</h2>
+      </div>
     </div>
   )
 }
