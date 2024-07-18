04e3928488f2109b5306657f88c7ce1e4aa37879
Mohd Tanveer Khan <khantanveer6077@gmail.com>
2024-07-19 00:16:04 +0530

    update

--- frontend/src/Components/About.jsx
+++ frontend/src/Components/About.jsx
@@ -7,10 +7,10 @@ const About = () => {
   return (
     <div id="About" className="">
       <div className="lg:px-56 px-10 lg:py-0 py-20 text-center gap-5 lg:text-start flex lg:flex-row flex-col-reverse justify-between lg:gap-28 items-center ">
-      <img src={pic} width={290} height={290} className="rounded-full border-2 p-1 border-fuchsia-500 pic_glow" alt="" />
-      <div className="h-full lg:py-40 flex flex-col justify-center lg:items-start items-center text-white ">
-        <h1 className="text-[52px] font-semibold mb-8 leading-normal text-fuchsia-500 uppercase">About Me</h1>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Non eveniet accusamus quis ipsum eum debitis repellat ducimus, explicabo sunt tempore, natus enim ad accusantium, minus aperiam corporis commodi optio saepe!</p>
+      <img data-aos="fade-down" src={pic} width={290} height={290} className="rounded-full border-2 p-1 border-fuchsia-500 pic_glow" alt="" />
+      <div  className="h-full lg:py-40 flex flex-col justify-center lg:items-start items-center text-white ">
+        <h1 data-aos="fade-right" className="text-[52px] font-semibold mb-8 leading-normal text-fuchsia-500 uppercase">About Me</h1>
+        <p data-aos="fade-left"> Lorem ipsum dolor sit amet consectetur adipisicing elit. Non eveniet accusamus quis ipsum eum debitis repellat ducimus, explicabo sunt tempore, natus enim ad accusantium, minus aperiam corporis commodi optio saepe!</p>
         <div className="flex mt-8 gap-2 ">
           <div className="flex items-center justify-center ">
             <div className="flex space-x-2 ">
--- frontend/src/Components/Banner.jsx
+++ frontend/src/Components/Banner.jsx
@@ -5,20 +5,23 @@ import { FaXTwitter } from "react-icons/fa6";
 import pic from '../images/pic.jpeg'
 import AOS from 'aos';
 import 'aos/dist/aos.css';
+import { useEffect } from "react";
 
 const Banner = () => {
 
-  AOS.init({
-    easing: 'ease-out-quart',
-    delay: 0,
-    duration: 750
-  })
-
+  useEffect(() => {
+    AOS.init({
+      easing: 'ease-out-quart',
+      delay: 0,
+      duration: 750
+    })
+  
+  },[])
   return (
     <div  className="lg:px-56 px-10 lg:py-0 py-20 text-center gap-5 lg:text-start flex lg:flex-row flex-col-reverse justify-between lg:gap-28 items-center ">
-      <div data-aos="fade-up" className="h-full lg:py-40 flex flex-col justify-center lg:items-start items-center text-white ">
-        <h1 className="text-[52px] font-semibold mb-8 leading-normal uppercase">Welcome To <span className="text-fuchsia-500 ">my Portfolio</span></h1>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Non eveniet accusamus quis ipsum eum debitis repellat ducimus, explicabo sunt tempore, natus enim ad accusantium, minus aperiam corporis commodi optio saepe!</p>
+      <div className="h-full lg:py-40 flex flex-col justify-center lg:items-start items-center text-white ">
+        <h1 data-aos="fade-right" className="text-[52px] font-semibold mb-8 leading-normal uppercase">Welcome To <span className="text-fuchsia-500 ">my Portfolio</span></h1>
+        <p data-aos="fade-left">Lorem ipsum dolor sit amet consectetur adipisicing elit. Non eveniet accusamus quis ipsum eum debitis repellat ducimus, explicabo sunt tempore, natus enim ad accusantium, minus aperiam corporis commodi optio saepe!</p>
         <div className="flex mt-8 gap-2 ">
           <div className="flex items-center justify-center ">
             <div className="flex space-x-2 ">
@@ -39,7 +42,7 @@ const Banner = () => {
           </div>
         </div>
       </div>
-      <img src={pic} width={290} height={290} className="rounded-full border-2 p-1 border-fuchsia-500 pic_glow" alt="" />
+      <img data-aos="fade-up" src={pic} width={290} height={290} className="rounded-full border-2 p-1 border-fuchsia-500 pic_glow" alt="" />
     </div>
   )
 }
