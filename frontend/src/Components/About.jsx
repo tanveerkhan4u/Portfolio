625122b9e0d40de23059890ce52cb86beb316bc7
Mohd Tanveer Khan <khantanveer6077@gmail.com>
2024-07-19 00:05:23 +0530

    update

--- frontend/src/Components/About.jsx
+++ frontend/src/Components/About.jsx
@@ -14,7 +14,7 @@ const About = () => {
         <div className="flex mt-8 gap-2 ">
           <div className="flex items-center justify-center ">
             <div className="flex space-x-2 ">
-             
+             <button className='neno-button shadow-xl hover:shadow-fuchsia-800/50 text-white border-2 hover:bg-fuchsia-800 border-fuchsia-800 rounded-lg py-4 px-8 uppercase relative overflow-hidden '>Resume</button>
             </div>
           </div>
         </div>
--- frontend/src/Components/Banner.jsx
+++ frontend/src/Components/Banner.jsx
@@ -3,14 +3,21 @@ import { FaLinkedinIn } from "react-icons/fa";
 import { FaInstagram } from "react-icons/fa";
 import { FaXTwitter } from "react-icons/fa6";
 import pic from '../images/pic.jpeg'
-
-
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 const Banner = () => {
+
+  AOS.init({
+    easing: 'ease-out-quart',
+    delay: 0,
+    duration: 750
+  })
+
   return (
-    <div className="lg:px-56 px-10 lg:py-0 py-20 text-center gap-5 lg:text-start flex lg:flex-row flex-col-reverse justify-between lg:gap-28 items-center ">
+    <div data-aos="fade-up" className="lg:px-56 px-10 lg:py-0 py-20 text-center gap-5 lg:text-start flex lg:flex-row flex-col-reverse justify-between lg:gap-28 items-center ">
       <div className="h-full lg:py-40 flex flex-col justify-center lg:items-start items-center text-white ">
-        <h1 className="text-[52px] font-semibold mb-8 leading-normal">Welcome To <span className="text-fuchsia-500 ">My Portfolio</span></h1>
+        <h1 className="text-[52px] font-semibold mb-8 leading-normal uppercase">Welcome To <span className="text-fuchsia-500 ">my Portfolio</span></h1>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Non eveniet accusamus quis ipsum eum debitis repellat ducimus, explicabo sunt tempore, natus enim ad accusantium, minus aperiam corporis commodi optio saepe!</p>
         <div className="flex mt-8 gap-2 ">
           <div className="flex items-center justify-center ">
