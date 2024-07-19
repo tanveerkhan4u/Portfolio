70a768852098fb865a057950df2536bed6a3a4d8
tanveerkhan4u <khantanveer6077@gmail.com>
2024-07-19 22:32:49 +0530

    update

--- frontend/src/Components/Banner.jsx
+++ frontend/src/Components/Banner.jsx
@@ -20,7 +20,7 @@ const Banner = () => {
   return (
     <div  className="lg:px-56 px-10 lg:py-0 py-20 text-center gap-5 lg:text-start flex lg:flex-row flex-col-reverse justify-between lg:gap-28 items-center ">
       <div className="h-full lg:py-40 flex flex-col justify-center lg:items-start items-center text-white ">
-        <h1 data-aos="fade-right" className="text-[52px] font-semibold mb-8 leading-normal uppercase">Welcome To <span className="text-fuchsia-500 ">my Portfolio</span></h1>
+        <h1 data-aos="fade-right" className="text-[52px] font-semibold mb-8 leading-normal uppercase">Welcome To <span className="text-fuchsia-500 ">my Portfoli</span></h1>
         <p data-aos="fade-left">Lorem ipsum dolor sit amet consectetur adipisicing elit. Non eveniet accusamus quis ipsum eum debitis repellat ducimus, explicabo sunt tempore, natus enim ad accusantium, minus aperiam corporis commodi optio saepe!</p>
         <div className="flex mt-8 gap-2 ">
           <div className="flex items-center justify-center ">
