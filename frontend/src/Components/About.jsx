91cc757a89bac1d110876719bc93039ac9b10dbf
tanveerkhan4u <khantanveer6077@gmail.com>
2024-07-20 03:02:59 +0530

    update

--- frontend/src/Components/About.jsx
+++ frontend/src/Components/About.jsx
@@ -10,7 +10,7 @@ const About = () => {
       <img data-aos="fade-down" src={pic} width={290} height={290} className="rounded-full border-2 p-1 border-fuchsia-500 pic_glow" alt="" />
       <div  className="h-full lg:py-40 flex flex-col justify-center lg:items-start items-center text-white ">
         <h1 data-aos="fade-right" className="text-[52px] font-semibold mb-8 leading-normal text-fuchsia-500 uppercase">About Me</h1>
-        <p data-aos="fade-left"> Lorem ipsum dolor sit amet consectetur adipisicing elit. Non eveniet accusamus quis ipsum eum debitis repellat ducimus, explicabo sunt tempore, natus enim ad accusantium, minus aperiam corporis commodi optio saepe!</p>
+        <p data-aos="fade-left"> Lorem ipsum  sit amet consectetur adipisicing elit. Non eveniet accusamus quis ipsum eum debitis repellat ducimus, explicabo sunt tempore, natus enim ad accusantium, minus aperiam corporis commodi optio saepe!</p>
         <div className="flex mt-8 gap-2 ">
           <div className="flex items-center justify-center ">
             <div className="flex space-x-2 ">
