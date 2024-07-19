fff1d448367b1b48f5042e5ec5d655ee5b4e946f
tanveerkhan4u <khantanveer6077@gmail.com>
2024-07-20 03:34:30 +0530

    update

--- Backend/index.js
+++ Backend/index.js
@@ -9,7 +9,7 @@ const port = 5000;
 
 app.use(express.json());
 app.use(cors({
-  origin: ['http://localhost:5173', 'https://portfolio-kov7.onrender.com']
+  origin: 'http://localhost:5173', 
 }));
 
 app.use('/contact', ContactRouter);
