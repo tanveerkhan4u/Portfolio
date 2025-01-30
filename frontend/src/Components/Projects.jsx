cd4bb8813754c5fe4c4b8a63c0f7c0c44a4f875d
tanveerkhan4u <khantanveer6077@gmail.com>
2025-01-30 16:30:44 +0530

    update

--- frontend/src/Components/Projects.jsx
+++ frontend/src/Components/Projects.jsx
@@ -2,6 +2,8 @@ import sun from "../images/sun.png";
 import code from "../images/code.png";
 import hand from "../images/hand.png";
 import screenshot from "../images/screenshot.png";
+import deep from "../images/deep.png";
+import otp from "../images/otp.png";
 
 
 const Projects = () => {
@@ -24,6 +26,12 @@ const Projects = () => {
         </a>
         <a href="https://github.com/tanveerkhan4u/Handicraft">
         <img data-aos="fade-up" src={hand} className="w-full h-full   flex items-center justify-center object-cover rounded-xl p-1  border-fuchsia-800 b_glow" alt="" />
+        </a>
+        <a href="https://www.deepcraneservices.com/">
+        <img data-aos="fade-up" src={deep} className="w-full h-full   flex items-center justify-center object-cover rounded-xl p-1  border-fuchsia-800 b_glow" alt="" />
+        </a>
+        <a href="https://tanveertravel.netlify.app/">
+        <img data-aos="fade-up" src={otp} className="w-full h-full   flex items-center justify-center object-cover rounded-xl p-1  border-fuchsia-800 b_glow" alt="" />
         </a>
       </div>
     </div>
